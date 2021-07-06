b7cd565d6649e298da3c31d923e01628df98b788
Stephen O'Connor <oconnor39@gmail.com>
2021-07-05 19:31:04 -0700

    Enable flask-gunicorn logging

--- hermit_tube/wsgi.py
+++ hermit_tube/wsgi.py
@@ -1,4 +1,12 @@
+# gunicorn wrapper
+
+import logging
 from hermit_tube.lib.wsgi.server import app
 
 if __name__ == '__main__':
     app.run()
+else:
+    # running in gunicorn context
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
