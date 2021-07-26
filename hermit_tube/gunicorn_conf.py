3e6e132b1960a169788d11430e785959b058a4a1
Stephen O'Connor <oconnor39@gmail.com>
2021-07-26 00:12:49 -0700

    Add gunicorn_conf.py

---
+++ hermit_tube/gunicorn_conf.py
@@ -0,0 +1,37 @@
+workers = 5
+
+raw_env = [
+    'PROMETHEUS_MULTIPROC_DIR=./prometheus_tmp',
+    ''
+]
+
+bind = 'unix:hermit-tube.socket'
+
+umask = '007'
+
+accesslog = '/var/www/hermit.tube/logs'
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+    
+def on_starting(server):
+    server.log.info("on_starting")
+
+def when_ready(server):
+    server.log.info("server ready")
+
+def pre_fork(server, worker):
+    worker.log.info("pre_fork")
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+def when_ready(server):
+    server.log.info("Server is ready. Spawning workers")
+
+def worker_int(worker):
+    worker.log.info("worker received INT or QUIT signal")
+
+def worker_exit(server, worker):
+    from prometheus_client import multiprocess
+    multiprocess.mark_process_dead(worker.pid)
