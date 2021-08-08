8940d840c52209366ffe74ac1f4fb128151d25d7
Stephen O'Connor <oconnor39@gmail.com>
2021-08-07 23:00:58 -0700

    Add scripts/deploy.py

---
+++ scripts/deploy.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+"""
+deploy.py - Increments version numbers, builds packages, and deploys to pypi.
+
+This file anticipates a json file in the root directory. This is not tracked
+in git to avoid "version bump" style commits (TODO: move file to cloud/server
+somewhere?). The file format is as follows:
+
+{
+  "current": "0.1.12dev4",
+  "dev": {
+    "label": "dev",
+    "version": [0, 1, 12, 4]
+  },
+  "master": {
+    "version": [0, 1, 11]
+  }
+}
+
+Other prerelease processes should be possible, but dev is enough for me. This
+also works to keep dev/master versioning aligned.
+"""
+
+
+import argparse
+import json
+import glob
+import sys
+import time
+import os
+
+from pygit2 import Repository
+import build.__main__ as build
+import twine.__main__ as twine
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--major', action='store_true')
+parser.add_argument('--minor', action='store_true')
+parser.add_argument('--patch', action='store_true')
+parser.add_argument('--skip_inc', action='store_true', help='Do not increment version')
+parser.add_argument('--keep', action='store_true', help='Do not delete old packages')
+parser.add_argument('--build', action='store_true', help='Run build util')
+parser.add_argument('--deploy', action='store_true', help='Upload packags')
+args = parser.parse_args()
+
+repo = Repository('.')
+branch = repo.head.name.split('/')[-1]
+
+print(f'Working from branch {branch}')
+
+with open('versions.json', 'r') as fp:
+    versions = json.load(fp)
+
+if branch not in versions:
+    print('Branch not recognized')
+    sys.exit(1)
+
+version = versions[branch]['version']
+master = versions['master']['version']
+
+if branch != 'master' and  tuple(master) >= tuple(version[0:3]):
+    for i, n in enumerate(master):
+        version[i] = master[i]
+    version[-1] = 0
+    version[-2] += 1
+
+
+def inc(version: list[int], index: int):
+    version[index] += 1
+    for i in range(index+1, len(version)):
+        version[i] = 0
+
+if not args.skip_inc:
+    if args.major:
+        inc(version, 0)
+    elif args.minor:
+        inc(version, 1)
+    elif args.minor:
+        inc(version, 2)
+    else:
+        inc(version, len(version)-1)
+
+versions['current'] = '.'.join(str(x) for x in version[0:3])
+if versions[branch].get('label'):
+    versions['current'] += versions[branch]['label'] + str(version[3])
+
+print(f'Using version {versions["current"]}')
+
+print()
+for i in range(3):
+    print(f'{3-i}!', end='')
+    for i in range(10):
+        print('.', end='')
+    print('\r')
+print('Let\'s Go!')
+
+with open('versions.json', 'w') as fp:
+    fp.write(json.dumps(versions, indent=2))
+
+if not args.keep:
+    for filename in glob.glob('dist/*'):
+        os.remove(filename)
+
+if args.build:
+    build.main([])
+
+if args.deploy:
+    sys.argv = ['', 'upload', '-r', 'cubedtube', 'dist/*']
+    twine.main()
